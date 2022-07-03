f2f77812f467f1c2458a76c8c291acad8bb77133
jørgen buholm johansen <jorgen_buholm_johansen@hotmail.com>
2022-07-03 21:00:23 +0200

    added a single record andcreate pizza form

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,15 +1,27 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            Add a new Pizza
-        </div>
-
-        
-    
-        
-    </div>
+<div class="wrapper create-pizza">
+    <h1>Create a New Pizza</h1>
+    <form action="/pizzas" method="POST">
+        <label for="name">Your name:</label>
+        <!-- type for HTML5, id for JS/CSS, name for post requests/php-->
+        <input type="text" id="name" name="name">
+        <label for="type">Choose pizza type</label>
+        <select name="type" id="type">
+            <option value="margherita">Margherita</option>
+            <option value="hawaiian">Hawaiian</option>
+            <option value="veg supreme">Veg Supreme</option>
+            <option value="volcano">Volcano</option>
+        </select>
+        <label for="type">Choose pizza base</label>
+        <select name="base" id="base">
+            <option value="cheesy crust">Cheesy Crust</option>
+            <option value="garlic crust">Garlic Crust</option>
+            <option value="thin & crispy">Thin & Crispy</option>
+            <option value="thick">Thick</option>
+        </select>
+        <input type="submit" value="Order Pizza">
+    </form>
 </div>
 @endsection
